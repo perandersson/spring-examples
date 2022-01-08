05bac32946cf90fffa8d1f6dce0d59579daec499
Per Andersson <per@westcoastcode.se>
2022-01-08 11:22:25 +0100

    Added example1

---
+++ example1/README.md
@@ -0,0 +1,23 @@
+# Example1
+
+Empty Project
+
+## Build
+
+```bash
+mvn install
+```
+
+Built file can be found in: target/<artifact>-<groupid>-<version>.jar
+
+## Run
+
+In terminal:
+
+```bash
+java -jar target/se.westcoastcode-example1-1.0-SNAPSHOT.jar
+```
+
+In editor:
+
+Run **se.westcoastcode.example1.Application.main**
---
+++ example1/src/main/java/se/westcoastcode/example1/Application.java
@@ -0,0 +1,11 @@
+package se.westcoastcode.example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Example1Application.class, args);
+    }
+}
