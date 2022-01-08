470a22b1e9c57ec99b2dc2c7c5ad39a2b28bb817
Per Andersson <per@westcoastcode.se>
2022-01-08 11:38:42 +0100

    Documentation

--- example1/src/main/java/se/westcoastcode/example1/Application.java
+++ example1/src/main/java/se/westcoastcode/example1/Application.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * The @SpringBootApplication tells maven that this class
+ * The @SpringBootApplication annotation is scanned for by Spring Boot's build plugin.
+ * It configures the jar file so that java knows that this class contains the starting point
+ * of your application
  */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        // Let spring boot take over the execution
         SpringApplication.run(Example1Application.class, args);
     }
 }
